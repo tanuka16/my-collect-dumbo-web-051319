31788c41a40381f571d2a51037ca0924eaa79a20
Tanuka Das <das.tanuka@yahoo.com>
2019-05-01 04:14:59 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -9,19 +9,3 @@ def my_collect(collection)
 
   collect
 end
-
-
-def my_collect(array)
-   new_arr = []
-
-   i=0
-   while i < array.length
-   new_arr.push yield(array[i])
-   i += 1
- end
-   # for element in array
-#   #   new_arr.push yield element
-#   # end
-
-   new_arr
- end
