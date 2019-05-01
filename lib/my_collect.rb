50139b560447a0d4f3f12fcbc046b427b189c010
Tanuka Das <das.tanuka@yahoo.com>
2019-05-01 04:14:29 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -3,9 +3,25 @@ def my_collect(collection)
   i = 0
 
   while i < collection.length
-    collect<< yield(collection[i])
+    collect.push yield(collection[i])
     i += 1
   end
 
   collection
 end
+
+
+def my_collect(array)
+   new_arr = []
+
+   i=0
+   while i < array.length
+   new_arr.push yield(array[i])
+   i += 1
+ end
+   # for element in array
+#   #   new_arr.push yield element
+#   # end
+
+   new_arr
+ end 
